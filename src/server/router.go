d72364da483ee8ff89826eb6aa3574f3b6e6ef3d
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2019-04-17 12:38:51 +0200

    group authenticated routes;

--- src/server/router.go
+++ src/server/router.go
@@ -1,15 +1,20 @@
 package server
 
 import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/Electra-project/electrapay-api/src/authenticators"
 	"github.com/Electra-project/electrapay-api/src/controllers"
 	"github.com/Electra-project/electrapay-api/src/middlewares"
-	"github.com/gin-gonic/gin"
 )
 
-// Router binds the routes to the controllers.
 func Router() *gin.Engine {
+
+	authenticator := authenticators.Authenticator()
+
 	router := gin.Default()
 	router.Use(middlewares.CORS())
+	router.Use(middlewares.ResponseHeaders())
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -18,41 +23,57 @@ func Router() *gin.Engine {
 		})
 	})
 
+	/**
+	 * public routes
+	 */
 	accountController := new(controllers.AccountController)
-	//public
-	router.POST("/v1/account/", accountController.New)
-	router.POST("/account/", accountController.New)
+
+	// account
 	router.POST("/v1/account/:uuid/register/", accountController.Register)
 	router.POST("/account/:uuid/register/", accountController.Register)
 
-	router.GET("/v1/account/:uuid", accountController.Get)
-	router.GET("/account/:uuid", accountController.Get)
-	router.PUT("/v1/account/:uuid", accountController.Edit)
-	router.PUT("/account/:uuid", accountController.Edit)
-	router.PUT("/v1/account/:uuid/close", accountController.Close)
-	router.PUT("/account/:uuid/close", accountController.Close)
-	router.PUT("/v1/account/:uuid/address/:addressid", accountController.AddressEdit)
-	router.PUT("/account/:uuid/address/:addressid", accountController.AddressEdit)
-	router.PUT("/v1/account/:uuid/address", accountController.AddressAdd)
-	router.PUT("/account/:uuid/address", accountController.AddressAdd)
-	router.DELETE("/v1/account/:uuid/address/:addressid", accountController.AddressRemove)
-	router.DELETE("/account/:uuid/address/:addressid", accountController.AddressRemove)
-	router.PUT("/v1/account/:uuid/contact/:contactid", accountController.ContactEdit)
-	router.PUT("/account/:uuid/contact/:contactid", accountController.ContactEdit)
-	router.PUT("/v1/account/:uuid/contact/", accountController.ContactAdd)
-	router.PUT("/account/:uuid/contact/", accountController.ContactAdd)
-	router.DELETE("/v1/account/:uuid/contact/:contactid", accountController.ContactRemove)
-	router.DELETE("/account/:uuid/contact/:contactid", accountController.ContactRemove)
+	// login
+	router.POST("/v1/authenticate", authenticator.LoginHandler)
+	router.POST("/authenticate", authenticator.LoginHandler)
 
-	orderController := new(controllers.OrderController)
-	router.POST("/v1/order/", orderController.New)
-	router.POST("/order/", orderController.New)
-	router.GET("/v1/order/:uuid", orderController.Get)
-	router.GET("/order/:uuid/", orderController.Get)
-	router.POST("/v1/order/:uuid/cancel", orderController.Cancel)
-	router.POST("/order/:uuid/cancel", orderController.Cancel)
-	router.POST("/v1/order/:uuid/reverse", orderController.Reverse)
-	router.POST("/order/:uuid/reverse", orderController.Reverse)
+	/**
+	 * authenticated routes
+	 */
+	auth := router.Group("/")
+	auth.Use(authenticator.MiddlewareFunc())
+	{
+		auth.POST("/v1/account/", accountController.New)
+		auth.POST("/account/", accountController.New)
+
+		auth.GET("/v1/account/:uuid", accountController.Get)
+		auth.GET("/account/:uuid", accountController.Get)
+		auth.PUT("/v1/account/:uuid", accountController.Edit)
+		auth.PUT("/account/:uuid", accountController.Edit)
+		auth.PUT("/v1/account/:uuid/close", accountController.Close)
+		auth.PUT("/account/:uuid/close", accountController.Close)
+		auth.PUT("/v1/account/:uuid/address/:addressid", accountController.AddressEdit)
+		auth.PUT("/account/:uuid/address/:addressid", accountController.AddressEdit)
+		auth.PUT("/v1/account/:uuid/address", accountController.AddressAdd)
+		auth.PUT("/account/:uuid/address", accountController.AddressAdd)
+		auth.DELETE("/v1/account/:uuid/address/:addressid", accountController.AddressRemove)
+		auth.DELETE("/account/:uuid/address/:addressid", accountController.AddressRemove)
+		auth.PUT("/v1/account/:uuid/contact/:contactid", accountController.ContactEdit)
+		auth.PUT("/account/:uuid/contact/:contactid", accountController.ContactEdit)
+		auth.PUT("/v1/account/:uuid/contact/", accountController.ContactAdd)
+		auth.PUT("/account/:uuid/contact/", accountController.ContactAdd)
+		auth.DELETE("/v1/account/:uuid/contact/:contactid", accountController.ContactRemove)
+		auth.DELETE("/account/:uuid/contact/:contactid", accountController.ContactRemove)
+
+		orderController := new(controllers.OrderController)
+		auth.POST("/v1/order/", orderController.New)
+		auth.POST("/order/", orderController.New)
+		auth.GET("/v1/order/:uuid", orderController.Get)
+		auth.GET("/order/:uuid/", orderController.Get)
+		auth.POST("/v1/order/:uuid/cancel", orderController.Cancel)
+		auth.POST("/order/:uuid/cancel", orderController.Cancel)
+		auth.POST("/v1/order/:uuid/reverse", orderController.Reverse)
+		auth.POST("/order/:uuid/reverse", orderController.Reverse)
+	}
 
 	router.NoRoute(func(c *gin.Context) {
 		c.JSON(404, gin.H{"message": "Not Found"})
