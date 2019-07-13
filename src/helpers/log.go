92130608986d304eb83d01284927d3bac56aa56a
Ruanne <ruanne.lloyd@multiply.co.za>
2019-07-13 14:45:20 +0200

    Add datetime stamps to logs

--- src/helpers/log.go
+++ src/helpers/log.go
@@ -3,24 +3,34 @@ package helpers
 import (
 	"fmt"
 	"github.com/ttacon/chalk"
+	"os"
+	"time"
 )
 
 // Log logs in green.
 func Log(s string) {
-	fmt.Println(chalk.Green.Color(s))
+	service := os.Getenv("GO_SERVICE")
+	t := time.Now()
+	fmt.Println(chalk.Green.Color(t.Format("2006-01-02 15:04:05") + ":" + service + ":" + s))
 }
 
 // LogErr logs in red.
 func LogErr(s string) {
-	fmt.Println(chalk.Red.Color(s))
+	service := os.Getenv("GO_SERVICE")
+	t := time.Now()
+	fmt.Println(chalk.Red.Color(t.Format("2006-01-02 15:04:05") + ":" + service + ":" + s))
 }
 
 // LogInfo logs in magenta.
 func LogInfo(s string) {
-	fmt.Println(chalk.Magenta.Color(s))
+	service := os.Getenv("GO_SERVICE")
+	t := time.Now()
+	fmt.Println(chalk.Magenta.Color(t.Format("2006-01-02 15:04:05") + ":" + service + ":" + s))
 }
 
 // LogWarn logs in yellow.
 func LogWarn(s string) {
-	fmt.Println(chalk.Yellow.Color(s))
+	service := os.Getenv("GO_SERVICE")
+	t := time.Now()
+	fmt.Println(chalk.Yellow.Color(t.Format("2006-01-02 15:04:05") + ":" + service + ":" + s))
 }
