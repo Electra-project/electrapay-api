0b68a944d59f859b597ee2456e5e4798d1023adc
Ruanne <ruannelloyd@gmail.com>
2019-10-05 22:23:10 +0200

    Add User Edit and User Get

--- src/models/user.go
+++ src/models/user.go
@@ -1,11 +1,23 @@
 package models
 
 type UserInfo struct {
-	Email               string `json:"email"`
-	Firstname           string `json:"firstname"`
-	Lastname            string `json:"lastname"`
-	Avatar              string `json:"avatar"`
-	Status              string `json:"status"`
+	Email               string           `json:"email"`
+	Firstname           string           `json:"firstname"`
+	Lastname            string           `json:"lastname"`
+	Avatar              string           `json:"avatar"`
+	Status              string           `json:"status"`
+	AccountInfo         []AccountDisplay `json:"accountinfo"`
+	ResponseCode        string           `json:"responsecode"`
+	ResponseDescription string           `json:"responsedescription"`
+}
+
+type AccountDisplay struct {
+	Id                  int64  `json:"id"`
+	Name                string `json:"name"`
+	Description         string `json:"description"`
+	Website             string `json:"website"`
+	WalletAddress       string `json:"walletaddress"`
+	WalletCurrency      string `json:"walletcurrency"`
 	ResponseCode        string `json:"responsecode"`
 	ResponseDescription string `json:"responsedescription"`
 }
